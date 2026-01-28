9cabc22fcc7efffe8b16aea94ef3d8aac5d7e57f
k1026 <southjiro2@gmail.com>
2026-01-28 11:30:36 +0900

    ・実装コードを一旦全削除

--- app/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/**
- * TailwindCSSのクラス名を結合・最適化するユーティリティ関数
- */
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
